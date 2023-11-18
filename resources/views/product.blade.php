0b236f73fb0ffc0f422fe54d4f5ba2122dd133e8
frank2720 <otienof534@gmail.com>
2023-11-18 20:51:30 +0300

    product details routing

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,7 +2,7 @@
 	@include('layouts.header')
 	<!-- component -->
 <style>@import url(https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css);</style>
-<div class="w-full max-w-6xl rounded bg-white shadow-xl p-10 mx-auto text-gray-800 relative md:text-left mt-2">
+<div class="w-full max-w-6xl rounded bg-white p-10 mx-auto text-gray-800 relative md:text-left mt-4">
     <div class="md:flex items-center">
         <div class="w-full md:w-1/2 px-10 mb-10 md:mb-0">
             <div class="relative">
