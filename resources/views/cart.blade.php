df92a78587cdbc2236069f116f0816d3808c6d3e
frank2720 <otienof534@gmail.com>
2023-11-24 00:02:20 +0300

    components

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,36 +6,7 @@
 
             @if (session()->has('cart'))
                 @foreach ($products as $product)
-                <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
-                    <img src="{{asset($product['item']->img)}}" alt="product-image" class="rounded-lg sm:w-40" />
-                    <div class="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                      <div class="mt-5 sm:mt-0">
-                        <h2 class="text-lg font-bold text-gray-900">{{$product['item']->name}}</h2>
-                        <p class="mt-1 text-xs text-gray-700">Ksh {{number_format($product['item']->price)}}</p>
-                      </div>
-                      <div class="mt-4 flex justify-between im sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                        <!-- Input Number -->
-                          <div class="py-2 px-3 inline-block bg-white border border-gray-200 rounded-lg dark:bg-slate-900 dark:border-gray-700" data-hs-input-number>
-                              <div class="flex items-center gap-x-1.5">
-                                <button type="button" class="w-6 h-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-input-number-decrement>
-                                <svg class="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/></svg>
-                                </button>
-                                <input class="p-0 w-6 bg-transparent border-0 text-gray-800 text-center focus:ring-0 dark:text-white" type="number" placeholder="0" value="{{$product['qty']}}" data-hs-input-number-input>
-                                <button type="button" class="w-6 h-6 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" data-hs-input-number-increment>
-                                <svg class="flex-shrink-0 w-3.5 h-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
-                                </button>
-                              </div>
-                          </div>
-                        <!-- End Input Number -->
-                        <div class="flex items-center space-x-4">
-                          <p class="text-sm">Ksh {{number_format($product['price'])}}</p>
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 cursor-pointer duration-150 hover:text-red-500">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                          </svg>
-                        </div>
-                      </div>
-                    </div>
-                </div>
+                  <x-cart-products :product="$product"/>
                 @endforeach
             @else
             <div class="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
