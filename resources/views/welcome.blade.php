3ae0a6d7fed65e6dacb54536cad59de6714390aa
frank2720 <otienof534@gmail.com>
2023-12-17 10:58:38 +0300

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,25 +36,41 @@
             }
         </style>
     </head>
-<body>
+<body class="antialiased">
+  <div class="min-h-screen bg-gray-100">
     <nav class="fixed flex justify-between py-6 w-full lg:px-48 md:px-12 px-4 content-center bg-secondary z-10">
         <div class="flex items-center">
             <img src="{{asset('images/pudfralogo.svg')}}" alt="Logo" class="h-8" />
         </div>
-        <ul class="font-montserrat items-center hidden md:flex">
+        <ul class="items-center hidden md:flex">
             <li class="mx-3 ">
-              <a class="growing-underline" href="howitworks">
-                How it works
+              <a class="growing-underline" href="{{route('welcome')}}">
+                Home
               </a>
             </li>
             <li class="growing-underline mx-3">
-              <a href="features">Features</a>
+              <a href="{{route('shop')}}">Shop</a>
             </li>
             <li class="growing-underline mx-3">
-              <a href="pricing">Pricing</a>
+              <a href="{{route('shop')}}">Categories</a>
+            </li>
+            <li class="growing-underline mx-3">
+              <a href="{{route('shop')}}">New Arrivals</a>
+            </li>
+            <li class="growing-underline mx-3">
+              <a href="{{route('shop')}}">Deals</a>
+            </li>
+            <li class="growing-underline mx-3">
+              <a href="{{route('shop')}}">About Us</a>
+            </li>
+            <li class="growing-underline mx-3">
+              <a href="{{route('shop')}}">Contact Us</a>
             </li>
           </ul>
-          <div class="font-montserrat hidden md:block">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path  class="dark:fill-white" fill-rule="evenodd" clip-rule="evenodd" d="M16.9303 7C16.9621 6.92913 16.977 6.85189 16.9739 6.77432H17C16.8882 4.10591 14.6849 2 12.0049 2C9.325 2 7.12172 4.10591 7.00989 6.77432C6.9967 6.84898 6.9967 6.92535 7.00989 7H6.93171C5.65022 7 4.28034 7.84597 3.88264 10.1201L3.1049 16.3147C2.46858 20.8629 4.81062 22 7.86853 22H16.1585C19.2075 22 21.4789 20.3535 20.9133 16.3147L20.1444 10.1201C19.676 7.90964 18.3503 7 17.0865 7H16.9303ZM15.4932 7C15.4654 6.92794 15.4506 6.85153 15.4497 6.77432C15.4497 4.85682 13.8899 3.30238 11.9657 3.30238C10.0416 3.30238 8.48184 4.85682 8.48184 6.77432C8.49502 6.84898 8.49502 6.92535 8.48184 7H15.4932ZM9.097 12.1486C8.60889 12.1486 8.21321 11.7413 8.21321 11.2389C8.21321 10.7366 8.60889 10.3293 9.097 10.3293C9.5851 10.3293 9.98079 10.7366 9.98079 11.2389C9.98079 11.7413 9.5851 12.1486 9.097 12.1486ZM14.002 11.2389C14.002 11.7413 14.3977 12.1486 14.8858 12.1486C15.3739 12.1486 15.7696 11.7413 15.7696 11.2389C15.7696 10.7366 15.3739 10.3293 14.8858 10.3293C14.3977 10.3293 14.002 10.7366 14.002 11.2389Z" fill="#200E32" />
+          </svg>
+          <div class="hidden md:block">
             <button class="mr-6">Login</button>
             <button class="py-2 px-4 text-white bg-black rounded-3xl">
               Signup
@@ -68,7 +84,7 @@
         <div id="hideMenu" class="flex justify-end">
             <img src="{{asset('assets/images/logos/Cross.svg')}}" alt="" class="h-16 w-16" />
         </div>
-          <ul class="font-montserrat flex flex-col mx-8 my-24 items-center text-3xl">
+          <ul class="flex flex-col mx-8 my-24 items-center text-3xl">
             <li class="my-6">
               <a href="howitworks">How it works</a>
             </li>
@@ -95,7 +111,7 @@
         As a Junior Laravel Developer, I'm excited to invite you to our digital marketplace where every line of code contributes 
         to a smoother, more delightful e-commerce journey.
     </p>
-    <div class="font-montserrat">
+    <div>
       <a href="{{route('shop')}}">
         <button class="px-6 py-4 border-2 border-black border-solid rounded-lg">
             Visit the Ecommerce-Shop
@@ -124,8 +140,8 @@
       <div class="border-2 rounded-full bg-secondary text-black h-12 w-12 flex justify-center items-center mb-3">
         1
       </div>
-      <h3 class="font-montserrat font-medium text-xl mb-2">Visit shop</h3>
-      <p class="text-center font-montserrat">
+      <h3 class="font-medium text-xl mb-2">Visit shop</h3>
+      <p class="text-center">
         Visit the products page where a list of Items are listed and add the product you want to cart.
       </p>
     </div>
@@ -133,8 +149,8 @@
       <div class="border-2 rounded-full bg-secondary text-black h-12 w-12 flex justify-center items-center mb-3">
         2
       </div>
-      <h3 class="font-montserrat font-medium text-xl mb-2">View Cart</h3>
-      <p class="text-center font-montserrat">
+      <h3 class="font-medium text-xl mb-2">View Cart</h3>
+      <p class="text-center">
        View the number of items added to cart. You can reduce, increase the quantity of the product you want to order.
       </p>
     </div>
@@ -142,8 +158,8 @@
       <div class="border-2 rounded-full bg-secondary text-black h-12 w-12 flex justify-center items-center mb-3">
         3
       </div>
-      <h3 class="font-montserrat font-medium text-xl mb-2">Place Order</h3>
-      <p class="text-center font-montserrat">
+      <h3 class="font-medium text-xl mb-2">Place Order</h3>
+      <p class="text-center">
         Place order for products in the cart and wait for the delivery.
       </p>
     </div>
@@ -151,7 +167,7 @@
 </section>
 
 <!-- About -->
-<section class="sectionSize bg-secondary">
+<section class="sectionSize bg-white">
     <h2 class="secondaryTitle bg-underline3 bg-100%">About</h2>
   <!-- features -->
   <div class="container py-5">
@@ -181,7 +197,7 @@
 </div>
 <!-- ./features -->
 
-    <div class="flex items-start font-montserrat">
+    <div class="flex items-start">
       <img src="{{asset('assets/images/logos/Heart.svg')}}" alt='' class="h-7 mr-4" />
       <div>
         <h3 class="font-semibold text-2xl"></h3>
@@ -202,10 +218,10 @@
 
 <!-- Available products -->
 <section class="sectionSize bg-secondary">
-    <h2 class="secondaryTitle bg-underline3 bg-100%">Products Overview</h2>
+    <h2 class="secondaryTitle bg-underline3 bg-100%">Featured Products</h2>
 </section>
 
-
+</div>
 <!-- Footer -->
 <section class="bg-white dark:bg-gray-900 sectionSize">
       <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
