77ed4ab1874538d79cc3b95c7b33edd8b195a768
frank2720 <otienof534@gmail.com>
2023-12-17 19:46:25 +0300

    navbar profile

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -33,13 +33,56 @@
         @endif
       </div>
     </a>
-      
-    <div class="hidden md:block">
-      <button class="mr-6">Login</button>
-      <button class="py-2 px-4 text-white bg-black rounded-3xl">
-        Signup
-      </button>
-    </div>
+    
+    @if (Route::has('login'))
+        @auth
+          <div>
+            <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+              <span class="sr-only">Open user menu</span>
+              <img class="w-8 h-8 rounded-full" src="{{asset('assets/images/3551739.jpg')}}" alt="user photo">
+            </button>
+          </div>
+          <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+            <div class="px-4 py-3" role="none">
+              <p class="text-sm text-gray-900 dark:text-white" role="none">
+                {{ucwords(strtolower(Auth::user()->name))}}
+              </p>
+              <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                {{strtolower(Auth::user()->email)}}
+              </p>
+            </div>
+            <ul class="py-1" role="none">
+              <li>
+                <a href="/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Profile</a>
+              </li>
+              <li>
+                <a href="/profile" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Orders</a>
+              </li>
+              <li>
+                <form method="POST" action="{{route('logout')}}">
+                  @csrf
+                <a href="{{route('logout')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem"
+                  onclick="event.preventDefault();
+                  this.closest('form').submit();">Sign out
+                </a>
+                </form>
+              </li>
+            </ul>
+          </div>
+        @else
+        <div class="hidden md:block">
+          <button class="mr-6"><a href="{{route('login')}}">Login</a></button>
+          @if (Route::has('register'))
+          <button class="py-2 px-4 text-white bg-black rounded-3xl">
+            <a href="{{route('register')}}">SignUp</a>
+          </button>
+          @endif
+        </div>
+        @endauth
+    @endif
     <div id="showMenu" class="md:hidden">
       <img src="{{asset('assets/images/logos/Menu.svg')}}" alt="Menu icon" />
     </div>
