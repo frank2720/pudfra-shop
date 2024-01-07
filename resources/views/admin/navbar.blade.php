6eddd19a0673d8e462b7b01b0c2f57da265a4d9c
frank2720 <otienof534@gmail.com>
2024-01-07 08:57:41 +0300

    admin

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -53,11 +53,6 @@
           <button class="menu-btn focus:outline-none focus:shadow-outline flex flex-wrap items-center">
             <div class="w-8 h-8 overflow-hidden rounded-full">
               <img class="w-full h-full object-cover" src="{{asset('assets/images/3551739.jpg')}}" >
-            </div> 
-
-            <div class="ml-2 capitalize flex ">
-              <h1 class="text-sm text-gray-800 font-semibold m-0 p-0 leading-none">moeSaid</h1>
-              <i class="fad fa-chevron-down ml-2 text-xs leading-none"></i>
             </div>                        
           </button>
 
@@ -67,29 +62,15 @@
 
             <!-- item -->
             <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="#">
-              <i class="fad fa-user-edit text-xs mr-1"></i> 
-              edit my profile
+              <i class="fa-regular fa-user mr-1"></i>
+              profile
             </a>     
             <!-- end item -->
 
             <!-- item -->
             <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="#">
-              <i class="fad fa-inbox-in text-xs mr-1"></i> 
-              my inbox
-            </a>     
-            <!-- end item -->
-
-            <!-- item -->
-            <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="#">
-              <i class="fad fa-badge-check text-xs mr-1"></i> 
-              tasks
-            </a>     
-            <!-- end item -->
-
-            <!-- item -->
-            <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out" href="#">
-              <i class="fad fa-comment-alt-dots text-xs mr-1"></i> 
-              chats
+              <i class="fa-solid fa-gear mr-1"></i>
+              account settings
             </a>     
             <!-- end item -->
 
@@ -109,8 +90,8 @@
         <!-- notifcation -->
         <div class="dropdown relative mr-5 md:static">
 
-          <button class="text-gray-700 menu-btn p-0 m-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none transition-all ease-in-out duration-300">
-            <i class="fad fa-bells"></i>               
+          <button class="text-gray-900 menu-btn p-0 m-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none transition-all ease-in-out duration-300">
+            <span><i class="fa-solid fa-comments"></i></span>             
           </button>
 
           <button class="hidden fixed top-0 left-0 z-10 w-full h-full menu-overflow"></button>
@@ -118,8 +99,8 @@
           <div class="menu hidden rounded bg-white md:right-0 md:w-full shadow-md absolute z-20 right-0 w-84 mt-5 py-2 animated faster">
             <!-- top -->
             <div class="px-4 py-2 flex flex-row justify-between items-center capitalize font-semibold text-sm">
-              <h1>notifications</h1>
-              <div class="bg-teal-100 border border-teal-200 text-teal-500 text-xs rounded px-1">
+              <h1>new notifications</h1>
+              <div class="bg-red-700 border border-red-200 text-white text-xs rounded px-1">
                 <strong>5</strong>
               </div>
             </div>
@@ -224,106 +205,7 @@
             <!-- end bottom -->            
           </div>
         </div>
-        <!-- end notifcation -->
-
-        <!-- messages -->
-        <div class="dropdown relative mr-5 md:static">
-
-          <button class="text-gray-700 menu-btn p-0 m-0 hover:text-gray-900 focus:text-gray-900 focus:outline-none transition-all ease-in-out duration-300">
-            <i class="fad fa-comments"></i>               
-          </button>
-
-          <button class="hidden fixed top-0 left-0 z-10 w-full h-full menu-overflow"></button>
-
-          <div class="menu hidden md:w-full md:right-0 rounded bg-white shadow-md absolute z-20 right-0 w-84 mt-5 py-2 animated faster">
-            <!-- top -->
-            <div class="px-4 py-2 flex flex-row justify-between items-center capitalize font-semibold text-sm">
-              <h1>messages</h1>
-              <div class="bg-teal-100 border border-teal-200 text-teal-500 text-xs rounded px-1">
-                <strong>3</strong>
-              </div>
-            </div>
-            <hr>
-            <!-- end top -->
-
-            <!-- body -->
-
-            <!-- item -->
-            <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
-
-              <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
-                <img class="w-full h-full object-cover" src="img/user1.jpg" alt="">
-              </div>
-
-              <div class="flex-1 flex flex-rowbg-green-100">
-                <div class="flex-1">
-                  <h1 class="text-sm font-semibold">mohamed said</h1>
-                  <p class="text-xs text-gray-500">yeah i know</p>
-                </div>
-                <div class="text-right text-xs text-gray-500">
-                  <p>4 min ago</p>
-                </div>
-              </div>
-
-            </a>
-            <hr>
-            <!-- end item --> 
-
-            <!-- item -->
-            <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
-
-              <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
-                <img class="w-full h-full object-cover" src="img/user2.jpg" alt="">
-              </div>
-
-              <div class="flex-1 flex flex-rowbg-green-100">
-                <div class="flex-1">
-                  <h1 class="text-sm font-semibold">sull goldmen</h1>
-                  <p class="text-xs text-gray-500">for sure</p>
-                </div>
-                <div class="text-right text-xs text-gray-500">
-                  <p>1 day ago</p>
-                </div>
-              </div>
-
-            </a>
-            <hr>
-            <!-- end item -->
-
-            <!-- item -->
-            <a class="flex flex-row items-center justify-start px-4 py-4 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 transition-all duration-300 ease-in-out" href="#">
-
-              <div class="w-10 h-10 rounded-full overflow-hidden mr-3 bg-gray-100 border border-gray-300">
-                <img class="w-full h-full object-cover" src="img/user3.jpg" alt="">
-              </div>
-
-              <div class="flex-1 flex flex-rowbg-green-100">
-                <div class="flex-1">
-                  <h1 class="text-sm font-semibold">mick</h1>
-                  <p class="text-xs text-gray-500">is typing ....</p>
-                </div>
-                <div class="text-right text-xs text-gray-500">
-                  <p>31 feb</p>
-                </div>
-              </div>
-
-            </a>
-            <!-- end item -->
-
-
-            <!-- end body -->
-
-            <!-- bottom -->
-            <hr>
-            <div class="px-4 py-2 mt-2">
-              <a href="#" class="border border-gray-300 block text-center text-xs uppercase rounded p-1 hover:text-teal-500 transition-all ease-in-out duration-500">
-                view all
-              </a>
-            </div>
-            <!-- end bottom -->            
-          </div>
-        </div>
-        <!-- end messages -->               
+        <!-- end notifcation -->             
 
 
       </div>
