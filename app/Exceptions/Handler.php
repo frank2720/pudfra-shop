a395d772fe87e0c9fc4ce17bb6e29c6d1d5c6559
frank2720 <otienof534@gmail.com>
2024-01-28 17:55:40 +0300

    authorization

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -27,4 +27,20 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function render($request, Throwable $e)
+    {
+         if ($this->isHttpException($e)) {
+            if ($e->getStatusCode() == 404) {
+                return response()->view('errors.404', [], 404);
+            }
+            if ($e->getStatusCode() == 403) {
+                return response()->view('errors.403',[], 403);
+            }
+            if ($e->getStatusCode() == 500) {
+                return response()->view('errors.500', [], 500);
+            }
+         }
+        return parent::render($request, $e);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,24 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::middleware(['auth,verified'])->group(function () {
-      Route::get('/dashboard');
+Route::middleware(['auth','verified'])->group(function () {
+
+      Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+      Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+      Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+      Route::group([
+            'prefix'=> 'admin',
+            'middleware'=>'is_admin',
+      ], function() {
+            Route::get('dashboard', function(){
+                  return view('admin/dashboard');
+            })->name('admin.interphase');
+            Route::resource('products', ProductController::class)
+                  ->only(['create','store']);
+      });
 });
 
-/* 
-(client side) Routes
-*/
 Route::get('/', [ProductController::class,'recent_products'])->name('welcome');
 Route::get('/shop', [ProductController::class, 'products'])->name('shop');
 Route::get('/product/{id}', [ProductController::class, 'product'])->name('product_details');
@@ -30,27 +41,5 @@ Route::get('/cart/{id}', [ProductController::class, 'addToCart'])->name('addToCa
 Route::get('/shopping/cart', [ProductController::class, 'getCart'])->name('shopping');
 Route::get('/shopping/removeItem/{id}', [ProductController::class, 'removefromCart'])->name('removefromCart');
 Route::get('/shopping/reduceItem/{id}', [ProductController::class, 'reduceInCart'])->name('productReduce');
-
-/*Product Contoller routes 
-(admin side)
-*/
-Route::resource('products', ProductController::class)
-      ->only(['create','store'])
-      ->middleware(['auth','verified']);
-      
-Route::get('/maar-shop/admin-dash', function () {
-       return view('admin/dashboard');
-})->middleware(['auth','verified'])->name('admin.interphase');
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::resource('products', ProductController::class)
-      ->only(['create','store'])
-      ->middleware(['auth']);
 
 require __DIR__.'/auth.php';
