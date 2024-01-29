b926825b8a0e3e35e6af93ccd0b9545145c652f2
frank2720 <otienof534@gmail.com>
2024-01-29 10:16:11 +0300

    error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -9,134 +9,14 @@
 
   <title>500</title>
 
-  <style id="" media="all">
-    /* cyrillic-ext */
-    @font-face {
-      font-family: 'Montserrat';
-      font-style: normal;
-      font-weight: 900;
-      font-display: swap;
-      src: url(/fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCvC73w0aXpsog.woff2) format('woff2');
-      unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;
-    }
-
-    * {
-      -webkit-box-sizing: border-box;
-      box-sizing: border-box
-    }
-
-    body {
-      padding: 0;
-      margin: 0
-    }
-
-    #notfound {
-      position: relative;
-      height: 100vh
-    }
-
-    #notfound .notfound {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      -webkit-transform: translate(-50%, -50%);
-      -ms-transform: translate(-50%, -50%);
-      transform: translate(-50%, -50%)
-    }
-
-    .notfound {
-      max-width: 520px;
-      width: 100%;
-      line-height: 1.4;
-      text-align: center
-    }
-
-    .notfound .notfound-404 {
-      position: relative;
-      height: 240px
-    }
-
-    .notfound .notfound-404 h1 {
-      font-family: montserrat, sans-serif;
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      -webkit-transform: translate(-50%, -50%);
-      -ms-transform: translate(-50%, -50%);
-      transform: translate(-50%, -50%);
-      font-size: 252px;
-      font-weight: 900;
-      margin: 0;
-      color: #262626;
-      text-transform: uppercase;
-      letter-spacing: -40px;
-      margin-left: -20px
-    }
-
-    .notfound .notfound-404 h1>span {
-      text-shadow: -8px 0 0 #fff
-    }
-
-    .notfound .notfound-404 h3 {
-      font-family: cabin, sans-serif;
-      position: relative;
-      font-size: 16px;
-      font-weight: 700;
-      text-transform: uppercase;
-      color: #262626;
-      margin: 0;
-      letter-spacing: 3px;
-      padding-left: 6px
-    }
-
-    .notfound h2 {
-      font-family: cabin, sans-serif;
-      font-size: 20px;
-      font-weight: 400;
-      text-transform: uppercase;
-      color: #000;
-      margin-top: 0;
-      margin-bottom: 25px
-    }
-
-    @media only screen and (max-width: 767px) {
-      .notfound .notfound-404 {
-        height: 200px
-      }
-
-      .notfound .notfound-404 h1 {
-        font-size: 200px
-      }
-    }
-
-    @media only screen and (max-width: 480px) {
-      .notfound .notfound-404 {
-        height: 162px
-      }
-
-      .notfound .notfound-404 h1 {
-        font-size: 162px;
-        height: 150px;
-        line-height: 162px
-      }
-
-      .notfound h2 {
-        font-size: 16px
-      }
-    }
-  </style>
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
   <meta name="robots" content="noindex, follow">
 </head>
-
-<body>
-  <div id="notfound">
-    <div class="notfound">
-      <div class="notfound-404">
-        <h3>Oops! We are experiencing server issue</h3>
-        <h1><span>5</span><span>0</span><span>0</span></h1>
-      </div>
-      <h2>we are sorry for any inconvince this might have cost we are working on it.</h2>
-    </div>
+<body class="flex flex-col h-screen justify-center items-center bg-gray-100">
+  <div class="flex flex-col items-center">
+      <h1 class="text-[120px] font-extrabold text-red-500">500</h1>
+      <p class="text-2xl font-medium text-gray-600">Internal Server Error</p>
+      <p class="text-2xl font-medium text-gray-600 mb-6">We apologize for the inconvenience. Please try again later.</p>
   </div>
 </body>
 
