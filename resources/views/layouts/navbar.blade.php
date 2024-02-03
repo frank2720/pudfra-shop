9c2a27e6b07525c829af6a787923fa820b6b7f84
frank2720 <otienof534@gmail.com>
2024-02-03 03:21:59 -0800

    pages display

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -19,7 +19,7 @@
             <!-- Left links -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
-                  <a class="nav-link " href="home.php">Home</a>
+                  <a class="nav-link " href="/">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="{{route('shop')}}">Products</a>
