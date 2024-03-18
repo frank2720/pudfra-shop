49c9f74d715452e567c9eac7715d3ccb0b639924
frank2720 <otienof534@gmail.com>
2024-03-18 15:03:33 +0300

    file compiling

--- resources/views/admin/addproduct.blade.php
+++ resources/views/admin/addproduct.blade.php
@@ -27,7 +27,7 @@
                     </div>
                     <div class="form-group">
                         <label>{{__('Image')}}</label>
-                        <input type="file" name="img[]" class="form-control" multiple>
+                        <input type="file" name="img[]" class="form-control" multiple required>
                     </div>					
                 </div>
                 <div class="modal-footer">
