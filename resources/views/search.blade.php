c5c70389b65c9d53e6fe3fd13060a450d8f4a490
frank2720 <otienof534@gmail.com>
2024-03-20 08:33:03 +0300

    search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -35,53 +35,18 @@
             <div class="search_header__content mini_cart_content fixcl-scroll widget">
                 <div class="fixcl-scroll-content product_list_widget">
                     <div class="js_prs_search" id="search_product">
-                        <div class="row mb__10 pb__10">
-                            <div class="col widget_img_pr">
-                                <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%201200%201200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="sunlight bell solar lamp" data-src="assets/images/mini-cart/product-01.jpg" width="80" height="80"></a>
-                            </div>
-                            <div class="col widget_if_pr">
-                                <a class="product-title db" href="product-detail-layout-01.html">sunlight bell solar lamp</a>$35.00
-                            </div>
-                        </div>
 
-                        <div class="row mb__10 pb__10">
-                            <div class="col widget_img_pr">
-                                <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20768%20768%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="cru thermos jug" data-src="assets/images/mini-cart/product-02.jpg" width="80" height="80"></a>
+                        @foreach ($latest as $product)
+                            <div class="row mb__10 pb__10">
+                                <div class="col widget_img_pr">
+                                    <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20768%20768%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="cru thermos jug" data-src="assets/images/mini-cart/product-02.jpg" width="80" height="80"></a>
+                                </div>
+                                <div class="col widget_if_pr">
+                                    <a class="product-title db" href="product-detail-layout-01.html">{{$product->name}}</a>
+                                    ${{$product->price}}
+                                </div>
                             </div>
-                            <div class="col widget_if_pr">
-                                <a class="product-title db" href="product-detail-layout-01.html">cru thermos jug</a>
-                                <del>$60.00</del>
-                                <ins>$45.00</ins>
-                                <span class="onsale nt_label">-25%</span>
-                            </div>
-                        </div>
-
-                        <div class="row mb__10 pb__10">
-                            <div class="col widget_img_pr">
-                                <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20768%20768%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="brush set small" data-src="assets/images/mini-cart/product-03.jpg" width="80" height="80"></a>
-                            </div>
-                            <div class="col widget_if_pr">
-                                <a class="product-title db" href="product-detail-layout-01.html">brush set small</a>$65.00
-                            </div>
-                        </div>
-
-                        <div class="row mb__10 pb__10">
-                            <div class="col widget_img_pr">
-                                <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20768%20768%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="stripe oilcloth" data-src="assets/images/mini-cart/product-04.jpg" width="80" height="80"></a>
-                            </div>
-                            <div class="col widget_if_pr">
-                                <a class="product-title db" href="product-detail-layout-01.html">stripe oilcloth</a>$35.00
-                            </div>
-                        </div>
-
-                        <div class="row mb__10 pb__10">
-                            <div class="col widget_img_pr">
-                                <a class="db pr oh" href="product-detail-layout-01.html"><img src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20768%20768%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" class="w__100 lz_op_ef lazyload" alt="picto wall clock" data-src="assets/images/mini-cart/product-05.jpg" width="80" height="80"></a>
-                            </div>
-                            <div class="col widget_if_pr">
-                                <a class="product-title db" href="product-detail-layout-01.html">picto wall clock</a>$15.00
-                            </div>
-                        </div>
+                        @endforeach
                         <a href="#" class="btn fwsb detail_link">View All
                             <i class="las la-arrow-right fs__18"></i></a>
                     </div>
