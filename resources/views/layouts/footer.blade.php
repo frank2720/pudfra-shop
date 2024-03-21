55c75dfd60e6ea89e93a7fb8801eed250ea536a6
frank2720 <otienof534@gmail.com>
2024-03-21 17:52:40 +0300

    unused files removal

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer -->
-<footer id="sticky-footer" class="flex-shrink-0 py-2 bg-dark text-white-50">
-    <!-- Copyright -->
-    <div class="container text-center">
-        <small>Copyright &copy; <a href="https://pudfra-talk.xyz/">Pudfra</a></small>
-    </div>
-    <!-- Copyright -->
-</footer>
