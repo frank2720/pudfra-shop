6904e41d65628e579ed434bf8edad963a2f1eb37
frank2720 <otienof534@gmail.com>
2024-03-23 05:28:10 +0300

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
-@section('content')
+@section('auth')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
