7ca63349e1d91e3c1c1db3feef6a388aaeb1d8af
frank2720 <otienof534@gmail.com>
2024-03-27 12:48:05 +0300

    unneccesary files

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,75 +1,22 @@
 @extends('layouts.main')
 @section('content')
-<!-- main slide -->
-<div class="nt_section type_slideshow type_carousel ">
-    <div class="slideshow-wrapper nt_full se_height_cus_h nt_first">
-        <div class="fade_flick_1 slideshow row no-gutters equal_nt nt_slider js_carousel prev_next_0 btn_owl_1 dot_owl_2 dot_color_1 btn_vi_2" data-flickity='{ "fade":0,"cellAlign": "center","imagesLoaded": 0,"lazyLoad": 0,"freeScroll": 0,"wrapAround": true,"autoPlay" : 0,"pauseAutoPlayOnHover" : true, "rightToLeft": false, "prevNextButtons": false,"pageDots": true, "contain" : 1,"adaptiveHeight" : 1,"dragThreshold" : 5,"percentPosition": 1 }'>
 
-            <!-- first slide -->
-            <div class="col-12 slideshow__slide">
-                <div class="oh position-relative nt_img_txt bg-black--transparent">
-                    <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
-                        <div class="bg_rp_norepeat bg_sz_cover lazyload item__position center center img_tran_ef pa l__0 t__0 r__0 b__0" data-bgset="assets/images/slide/slider-01.jpg"></div>
-                    </div>
-                    <div class="caption-wrap caption-w-1 pe_none z_100 tl_md tl">
-                        <div class="pa_txts caption kalles-caption-layout-01 kalles-caption--midle-left">
-                            <div class="left_right">
-                                <h4 class="kalles-caption-layout-01__subtitle mg__0 lh__1">SUMMER 2020</h4>
-                                <h3 class="kalles-caption-layout-01__title mg__0 lh__1">New Arrival Collection</h3>
-                                <a class="kalles-caption-layout-01__button kalles-button--square slt4_btn button pe_auto round_false btn_icon_false" href="">Explore Now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
-                </div>
+<div lc-helper="background" class="container-fluid py-5 mb-4 d-flex justify-content-center" style="  background-image: url('https://www.click.co.uk/wp-content/uploads/2021/09/Digital-Marketing-for-eCommerce-SEO-blog-hero-image.png');
+    background-position: center;
+    background-size:cover;
+    background-repeat:no-repeat">
+    <div class="p-5 mb-4 lc-block col-xxl-7 col-lg-8 col-12" style=" backdrop-filter: blur(6px) saturate(102%);
+        -webkit-backdrop-filter: blur(6px) saturate(102%);
+        background-color: rgba(255, 255, 255, 0.45);
+        border-radius: 12px;
+        border: 1px solid rgba(209, 213, 219, 0.3);">
+        <div class="lc-block">
+            <div editable="rich">
+                <h2 class="fw-bolder">Easy shopping & We deliver</h2>
             </div>
-            <!-- end first slide -->
-
-            <!-- second slide -->
-            <div class="col-12 slideshow__slide">
-                <div class="oh position-relative nt_img_txt bg-black--transparent">
-                    <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
-                        <div class="bg_rp_norepeat bg_sz_cover lazyload item__position center center img_zoom pa l__0 t__0 r__0 b__0" data-bgset="assets/images/slide/slider-02.jpg"></div>
-                    </div>
-                    <div class="caption-wrap caption-w-1 pe_none z_100 tr_md tl">
-                        <div class="pa_txts caption kalles-caption-layout-01 kalles-caption--midle-right">
-                            <div class="right_left">
-                                <h4 class="kalles-caption-layout-01__subtitle mg__0 lh__1">NEW SEASON</h4>
-                                <h3 class="kalles-caption-layout-01__title mg__0 lh__1">Lookbook Collection</h3>
-                                <a class="kalles-caption-layout-01__button kalles-button--square slt4_btn button pe_auto round_false btn_icon_false" href="">Explore Now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
-                </div>
-            </div>
-            <!-- end second slide -->
-
-            <!-- third slide -->
-            <div class="col-12 slideshow__slide">
-                <div class="oh position-relative nt_img_txt bg-black--transparent">
-                    <div class="js_full_ht4 img_slider_block kalles-slide-element__pdb-600">
-                        <div class="bg_rp_norepeat bg_sz_cover lazyload item__position center center img_tran_ef pa l__0 t__0 r__0 b__0" data-bgset="assets/images/slide/slider-03.jpg"></div>
-                    </div>
-                    <div class="caption-wrap caption-w-1 pe_none z_100 tl_md tl">
-                        <div class="pa_txts caption kalles-caption-layout-01 kalles-caption--midle-left">
-                            <div class="left_right">
-                                <h4 class="kalles-caption-layout-01__subtitle mg__0 lh__1">SUMMER SALE</h4>
-                                <h3 class="kalles-caption-layout-01__title mg__0 lh__1">Save up to 70%</h3>
-                                <a class="kalles-caption-layout-01__button kalles-button--square slt4_btn button pe_auto round_false btn_icon_false" href="">Explore Now</a>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="" class="pa t__0 l__0 b__0 r__0 pe_none"></a>
-                </div>
-            </div>
-            <!-- end third slide -->
-
         </div>
     </div>
 </div>
-<!-- end main slide -->
-
 <!-- featured collection -->
 <div class="nt_section type_featured_collection tp_se_cdt">
     <div class="kalles-otp-01__feature container">
