4c29e48ffbeef08f719fb6e91e8d5dbe0ef39ce2
frank2720 <otienof534@gmail.com>
2024-04-26 16:42:46 +0300

    dynamic title page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,6 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.guest')
+@section('title')
+    {{__('Verification')}}
+@endsection
+@section('auth')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
