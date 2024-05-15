6e18cbda65c327389c52aab7f04a23ffea3aa795
frank2720 <otienof534@gmail.com>
2024-05-15 19:40:13 +0300

    database seeder update

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,11 +21,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'user',
+            'email' => 'user@maanar.xyz',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'is_admin'=>1,
         ];
     }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,23 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        //\App\Models\User::factory(10)->create();
-
-        User::factory()->create([
-                'name' => 'admin',
-                'email' => 'otienof534@gmail.com',
-                'password'=>Hash::make('admin1234'),
-                'is_admin'=>1,
-            ]);
-
-        /**Product::create([
-            'name'=>'Lenovo ThinkPad',
-            'price'=>'150000',
-            'retail_price'=>'180000',
-            'description'=>' T14 Gen 2, Core i5 1135G7, 8GB, 512GB SSD, Windows 10 Pro, 14″ FHD Touch Screen 20W000S4UE',
-        ]);*/
-
-       // Product::factory(10)->create();
-        //Category::factory(7)->create();
+        $this->call([
+            UserSeeder::class
+        ]);
     }
 }
