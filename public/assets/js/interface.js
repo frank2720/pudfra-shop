2ba8bf163c6d55e180dc13b351ddece4e5aa8538
frank2720 <otienof534@gmail.com>
2024-05-16 13:13:44 +0300

    gift bug

--- public/assets/js/interface.js
+++ public/assets/js/interface.js
@@ -2464,18 +2464,11 @@ function addtocart(productId) {
 
         $('.fixcl-scroll-content').append(`
         <div class="mini_cart_tool js_cart_tool tc">
-            <div data-id="note" class="mini_cart_tool_note js_cart_tls ttip_nt tooltip_top">
-                <span class="txt_add_note "><i class="lar la-clipboard"></i><span class="tt_txt">Add Order Note</span></span>
-                <span class="txt_edit_note dn"><i class="las la-clipboard-list"></i><span class="tt_txt">Edit Order Note</span></span>
-            </div>
             <div data-id="gift" class="mini_cart_tool_gift js_cart_tls js_gift_wrap ttip_nt tooltip_top">
                 <i class="las la-gift"></i><span class="tt_txt">Add A Gift Wrap</span>
             </div>
             <div data-id="ship" class="mini_cart_tool_ship js_cart_tls ttip_nt tooltip_top">
                 <i class="las la-truck-moving"></i><span class="tt_txt">Estimate Shipping</span>
-            </div>
-            <div data-id="dis" class="mini_cart_tool_dis js_cart_tls ttip_nt tooltip_top">
-                <i class="las la-tag"></i><span class="tt_txt">Add A Coupon</span>
             </div>
         </div>`)
 
