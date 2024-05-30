c4609fe2728d6aac1205f3be4be96c7dfd1abd63
frank2720 <otienof534@gmail.com>
2024-05-30 23:50:04 +0300

    Towns Api buildup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            TownSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/TownSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Town;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TownSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Town::factory()->count(10)->create();
+    }
+}
