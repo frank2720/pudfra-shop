55ae50fca89bb4f25b3b67c1cf5a82eaec90a8e4
frank2720 <otienof534@gmail.com>
2024-06-02 21:00:55 +0300

    readme update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
         $towns = json_decode($json_data);
 
         $bestsales = Product::with('images')
-                        ->whereColumn('retail_price','>','price')
+                        ->inRandomOrder()
                         ->paginate(4);
 
         $latest = Product::with('images')
