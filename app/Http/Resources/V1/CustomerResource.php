68fd8736e66d4c932d80b333b4098f7d2b263d46
frank2720 <otienof534@gmail.com>
2024-06-03 10:36:40 +0300

    update put & patch

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -15,6 +15,7 @@ class CustomerResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id"=> $this->id,
             "name"=> $this->name,
             "type"=> $this->type,
             "email"=> $this->email,
--- app/Http/Resources/V1/TownResource.php
+++ app/Http/Resources/V1/TownResource.php
@@ -15,6 +15,7 @@ class TownResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            "id"=> $this->id,
             "town"=> $this->town,
             "latitude"=> $this->latitude,
             "longitude"=> $this->longitude,
