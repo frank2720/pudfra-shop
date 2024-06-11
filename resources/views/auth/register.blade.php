8b5ef6fdbe2d416a845b4890ca7b9c0f020855a7
frank2720 <otienof534@gmail.com>
2024-06-11 18:35:55 +0300

    login form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -34,7 +34,7 @@
     </div>
 
     <div class="form-group">
-        <input type="password" name="password_confirmation" class="form-control" placeholder="Password">
+        <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password">
     </div>
 
     <div class="form-group">
