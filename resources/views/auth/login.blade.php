706c93ad6061c3608a98dc32a48cceea9a086392
frank2720 <otienof534@gmail.com>
2024-06-11 18:31:02 +0300

    login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,141 +3,34 @@
     {{__('Login')}}
 @endsection
 @section('auth')
-<style>
-    .main-content{
-        width: 50%;
-        border-radius: 20px;
-        box-shadow: 0 5px 5px rgba(0,0,0,.4);
-        margin: 5em auto;
-        display: flex;
-    }
-    .company__info{
-        background-color: #008080;
-        border-top-left-radius: 20px;
-        border-bottom-left-radius: 20px;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        color: #fff;
-    }
-    .fa-android{
-        font-size:3em;
-    }
-    @media screen and (max-width: 640px) {
-        .main-content{width: 90%;}
-        .company__info{
-            display: none;
-        }
-        .login_form{
-            border-top-left-radius:20px;
-            border-bottom-left-radius:20px;
-        }
-    }
-    @media screen and (min-width: 642px) and (max-width:800px){
-        .main-content{width: 70%;}
-    }
-    .row > h2{
-        color:#008080;
-    }
-    .login_form{
-        background-color: #fff;
-        border-top-right-radius:20px;
-        border-bottom-right-radius:20px;
-        border-top:1px solid #ccc;
-        border-right:1px solid #ccc;
-    }
-    form{
-        padding: 0 2em;
-    }
-    .form__input{
-        width: 100%;
-        border:0px solid transparent;
-        border-radius: 0;
-        border-bottom: 1px solid #aaa;
-        padding: 1em .5em .5em;
-        padding-left: 2em;
-        outline:none;
-        margin:1.5em auto;
-        transition: all .5s ease;
-    }
-    .form__input:focus{
-        border-bottom-color: #008080;
-        box-shadow: 0 0 5px rgba(0,80,80,.4); 
-        border-radius: 4px;
-    }
-    .btn{
-        transition: all .5s ease;
-        width: 70%;
-        border-radius: 30px;
-        color:#008080;
-        font-weight: 600;
-        background-color: #fff;
-        border: 1px solid #008080;
-        margin-top: 1.5em;
-        margin-bottom: 1em;
-    }
-    .btn:hover, .btn:focus{
-        background-color: #008080;
-        color:#fff;
-    }
-</style>
-<div class="container">
-    <div class="row main-content bg-success text-center">
-        <div class="col-md-4 text-center company__info">
-            <span class="company__logo"><h2><span class="fa fa-android"></span></h2></span>
-            <h4 class="company_title">{{config('app.name')}}</h4>
-        </div>
-        <div class="col-md-8 col-xs-12 col-sm-12 login_form ">
-            <div class="container-fluid">
-                <div class="row">
-                    <h2>Log In</h2>
-                </div>
 
-                <div class="row">
-                    <form method="POST" action="{{ route('login') }}" class="form-group">
-                        @csrf
-
-                        <div class="row">
-                                <input id="email" type="email" class="form__input @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Email address" autocomplete="email" autofocus>
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-                        <div class="row">
-                                <input id="password" type="password" class="form__input @error('password') is-invalid @enderror" name="password" placeholder="password" autocomplete="current-password">
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        </div>
-
-                        <div class="row">
-                            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <label for="remember">
-                                {{ __('Remember Me') }}
-                            </label>
-                        </div>
-
-                        <div class="row">
-                            <input type="submit" value="Login" class="btn">
-                        </div>
-                    </form>
-                </div>
-                <div class="row">
-                    <p>Forgot Your Password? 
-                        @if (Route::has('password.request'))
-                            <a href="{{ route('password.request') }}">
-                                {{ __('Reset it here') }}
-                            </a>
-                        @endif
-                    </p>
-                </div>
-            </div>
-        </div>
+<form action="{{ route('login') }}" method="post">
+    @csrf
+    <h2 class="text-center">Log in</h2>       
+    <div class="form-group">
+        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email address" required="required">
+        @error('email')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
     </div>
-</div>
+    <div class="form-group">
+        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" required="required">
+        @error('password')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+    <div class="form-group">
+        <button type="submit" class="btn btn-primary btn-block">Log in</button>
+    </div>
+    <div class="clearfix">
+        <label class="float-left form-check-label"><input type="checkbox" name="remember"> Remember me</label>
+        <a href="{{ route('password.request') }}" class="float-right">Forgot Password?</a>
+    </div>        
+</form>
+<p class="text-center"><a href="{{route('register')}}">Create an Account</a></p>
+
 @endsection
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,27 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shortcut icon" type="image/png" href="{{Vite::asset('resources/assets/images/favicon.png')}}">
-    <title>@yield('title')</title>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="stylesheet" href="https://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
-    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>@yield('title')</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="shortcut icon" type="image/png" href="{{Vite::asset('resources/assets/images/favicon.png')}}">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style>
+    .auth-form {
+        width: 340px;
+        margin: 50px auto;
+        font-size: 15px;
+    }
+    .auth-form form {
+        margin-bottom: 15px;
+        background: #f7f7f7;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;
+    }
+    .auth-form h2 {
+        margin: 0 0 15px;
+    }
+    .form-control, .btn {
+        min-height: 38px;
+        border-radius: 2px;
+    }
+    .btn {        
+        font-size: 15px;
+        font-weight: bold;
+    }
+</style>
 </head>
 <body>
-    <div id="app">
-        <main class="py-4">
-            @yield('auth')
-        </main>
-    </div>
-    <script src="https://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
-    {!! Toastr::message() !!}
-    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    <script src="{{asset('assets/js/lazysizes.min.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+<div class="auth-form">
+    @yield('auth')
+</div>
 </body>
 </html>
