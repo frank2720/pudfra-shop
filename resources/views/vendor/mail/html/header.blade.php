3aa187633addfa92b50581239509a4f9f7487e58
frank2720 <otienof534@gmail.com>
2024-08-14 17:46:04 +0300

    assets building

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Maanar')
-<img src="https://maanar-shop.xyz/build/assets/logo-ed49ad7b.png" class="logo" alt="Maanar Logo">
+<img src="https://maanar-shop.xyz/build/assets/pudfra-0cc62e87.jpg" class="logo" alt="Maanar Logo">
 @else
 {{ $slot }}
 @endif
