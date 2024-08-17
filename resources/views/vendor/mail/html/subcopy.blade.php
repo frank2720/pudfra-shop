abed6e6488d895d4993b35c7c475804dbc07f407
frank2720 <otienof534@gmail.com>
2024-08-17 12:07:58 +0300

    vendor files

--- resources/views/vendor/mail/html/subcopy.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- start copy -->
-<tr>
-    <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 24px;">
-        {{ $slot }}
-    </td>
-</tr>
-<!-- end copy -->
