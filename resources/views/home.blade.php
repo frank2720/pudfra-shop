143790023df050aae14a62a092f59dcfac9d7bb1
frank2720 <otienof534@gmail.com>
2024-09-05 15:09:02 +0300

    assets build up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,56 +5,36 @@
 @section('content')
 
 <style>
-    .circle{border-radius: 50%;height:50px;width:50px;border:1px solid #ccc;
-    padding:0.7rem;text-align: center;}
-    .badge{position: absolute;top:0px; right:10px;}
-    hr{margin-bottom: 0rem !important;}
-    #nav li a{color:#000;}
-    #nav li{padding:0 10px;}
-    #nav li a:hover{color:#ccc;}
-    #hr{margin-top: 0rem !important;}
-    #car{border-radius: 12px;}
-    a:hover{text-decoration: none;}
-    #list-ul li a {color:#000;}
-    #list-ul .dropright .dropdown-toggle::after{margin-left: 160px !important;}
-    .card-body{padding:0 23px;}
-    .card h4{margin-top: -15px;}
-    .but{border-radius: 10px; padding:7px 15px;}
+    
+
+    
+   
+    
+   
     .col-md-3 .card:hover{box-shadow: 0 4px 15px rgb(153 153 153 / 30%)}
-    footer .col-md-3 ul li{list-style: none;}
-    footer .col-md-3 ul a{color:#000;}
+   
     h6{font-weight: bold}
-    .ok{background:#f8f9fa; padding-bottom: 20px;margin-top: 30px;}
     #cool{height:50px;width:100px;margin-left: 200px;}
     .color{background-color:lightblue;padding-top: 70px;padding-bottom: 70px;}
     .form{background-color: #fff;border:1px solid #fff;padding:20px;}
-    #circlee{position: relative;}
-    #login-badge{position: absolute;top:-5px;right:0px;}
 
-    .mytabs{margin-top:3rem;}
-    .mytabs .nav-link {
-    display: block;
-    padding: .5rem 9rem;border-bottom: 2px solid #000 !important;
-    font-weight: 700;
-    }
-    .mytabs .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
-
-    border-color: transparent !important;
-    color:#ff6600;
-    ;}
-    .wrap-text{padding: 1rem;}
+    
 </style>
-<div class = "col-md-12">
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner mt-2"id="car">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYKdc4-EM4JhSkmgKMC4nIuGKv_dexn7KyXw&usqp=CAU" alt="First slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://cdn.shopify.com/s/files/1/0074/0429/0111/files/Homepage-free-shipping-banner_1350x600.jpg?v=1591817221" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="https://i.pinimg.com/originals/df/c1/f9/dfc1f9ba2734aa94690f009d721440d7.jpg" alt="Third slide">
+<div class="">
+    <div class="row">
+        <div class = "col-md-6">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner mt-2" id="car">
+                    <div class="carousel-item active">
+                        <img class="w-100 h-75 d-inline-block" src="https://www.karkhanawala.in/wp-content/uploads/2020/04/Bags.jpg.webp" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="w-100 h-75 d-inline-block" src="https://rkgsbags.com/wp-content/uploads/slider/cache/80212ff7d9008ff5b19fd38750121179/slider2-3.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="w-100 h-75 d-inline-block" src="https://www.aryabags.com/uploads/1645192063.jpg" alt="Third slide">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -4,9 +4,9 @@
 @endsection
 @section('content')
 <div class = "container my-4">
-    <div class = "card">
+    <div class = "row">
       <!-- card left -->
-      <div class = "product-imgs">
+      <div class = "product-imgs col-md-6">
         <div class = "img-display ml-4">
           <div class = "img-showcase">
             @foreach ($product->images as $index => $image)
@@ -26,41 +26,63 @@
                   </a>
               </div>
           @endforeach
-      </div>
+        </div>
         @endif
       </div>
       <!-- card right -->
-      <div class = "product-content">
-        <h2 class = "product-name">{{$product->name}}</h2>
-        <div class = "product-rating">
-          <i class='bx bxs-star' ></i>
-          <i class='bx bxs-star' ></i>
-          <i class='bx bxs-star' ></i>
-          <i class='bx bxs-star' ></i>
-          <i class='bx bxs-star-half' ></i>
-          <span>4.7(21)</span>
-        </div>
-  
-        <div class = "product-price">
-            @if ($product->price<$product->retail_price)
-                <p class = "last-price">Old Price: <span>Ksh {{number_format($product->retail_price)}}</span></p>
-                <p class = "new-price">New Price: <span>Ksh {{number_format($product->price)}}</span></p>    
-            @else
-            <p class = "new-price">Price: <span>Ksh {{number_format($product->price)}}</span></p>
-            @endif
-        </div>
-  
-        <div class = "product-detail">
-          <h2>about this item: </h2>
-          <p>{!! $product->description !!}</p>
-        </div>
-  
-        <div class = "purchase-info">
-          <button type = "button" data-product-id="{{$product->id}}" class = "add-to-cart-btn">
-            Add to Cart <i class = "fas fa-shopping-cart"></i>
-          </button>
-        </div>
+      <div class = "product-content col-md-6">
+          <h2 class = "product-name">{{$product->name}}</h2>
+          <div class = "product-rating">
+            <i class='bx bxs-star' ></i>
+            <i class='bx bxs-star' ></i>
+            <i class='bx bxs-star' ></i>
+            <i class='bx bxs-star' ></i>
+            <i class='bx bxs-star-half' ></i>
+            <span>4.7(21)</span>
+          </div>
+    
+          <div class = "product-price">
+              @if ($product->price<$product->retail_price)
+                  <p class = "last-price">Old Price: <span>Ksh {{number_format($product->retail_price)}}</span></p>
+                  <p class = "new-price">New Price: <span>Ksh {{number_format($product->price)}}</span></p>    
+              @else
+              <p class = "new-price">Price: <span>Ksh {{number_format($product->price)}}</span></p>
+              @endif
+          </div>
+    
+          <div class = "product-detail">
+            <h2>about this item: </h2>
+            <p>{!! $product->description !!}</p>
+          </div>
+    
+          <div class = "purchase-info">
+            <button type = "button" data-product-id="{{$product->id}}" class = "add-to-cart-btn">
+              Add to Cart <i class = "fas fa-shopping-cart"></i>
+            </button>
+          </div>
       </div>
     </div>
-  </div>
+</div>
+
+<script>
+  const imgs = document.querySelectorAll('.img-select a');
+    const imgBtns = [...imgs];
+    let imgId = 1;
+
+    imgBtns.forEach((imgItem) => {
+        imgItem.addEventListener('click', (event) => {
+            event.preventDefault();
+            imgId = imgItem.dataset.id;
+            slideImage();
+        });
+    });
+
+    function slideImage(){
+        const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
+
+        document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+    }
+
+    window.addEventListener('resize', slideImage);
+</script>
 @endsection
