6bbfb6553b3a508c5fcd9471295cf00e12e4a82e
frank2720 <otienof534@gmail.com>
2024-09-18 16:43:33 +0300

    asset building

--- resources/views/trend.blade.php
+++ resources/views/trend.blade.php
@@ -2,18 +2,18 @@
     <div class="col-lg-3 col-md-3 col-6 pr_animated done mt__30 pr_grid_item product nt_pr desgin__1">
         <div class="product-inner pr">
             <div class="product-image position-relative oh lazyload">
-                @if ($product->retail_price>$product->price)
+               <!-- @if ($product->retail_price>$product->price)
                     <span class="tc nt_labels pa pe_none cw">
                         <span class="onsale nt_label">
                             <span>{{round((($product->price-$product->retail_price)/$product->retail_price)*100)}} %</span>
                         </span>
                     </span>
-                @endif
+                @endif -->
                 <a class="d-block" href="{{route('product.details',['id'=>$product->id])}}">
-                    <img src="{{Storage::url($product->images[0]->url??null)}}" class="main-img nt_img_ratio nt_bg_lz" alt="..">
+                    <img src="{{Storage::url($product->images[0]->url??null)}}" class="main-img nt_bg_lz img-same-height" alt="..">
                 </a>
                 <div class="hover_img pa pe_none t__0 l__0 r__0 b__0 op__0">
-                    <img src="{{Storage::url($product->images[1]->url??$product->images[0]->url??null)}}" class="back-img pa nt_bg_lz lazyload" alt="..">
+                    <img src="{{Storage::url($product->images[1]->url??$product->images[0]->url??null)}}" class="back-img pa nt_bg_lz lazyload img-same-height" alt="..">
                 </div>
                 <div class="hover_button op__0 tc pa flex column ts__03">
                     <a href="" class="pr pr_atc cd br__40 bgw tc dib js__qs cb chp ttip_nt tooltip_top_left add-to-cart-btn" data-product-id="{{$product->id}}">
@@ -24,8 +24,8 @@
                 </div>
             </div>
             <div class="product-info mt__15">
-                <h3 class="product-title position-relative fs__14 mg__0">
-                    <a class="cd chp" href="{{route('product.details',['id'=>$product->id])}}">{{__(ucfirst(strtolower($product->name)))}}</a>
+                <h3 class="product-title position-relative mg__0">
+                    <a class="cd chp max-lines" href="{{route('product.details',['id'=>$product->id])}}">{{__(ucfirst(strtolower($product->name)))}}</a>
                 </h3>
                 <span class="price dib mb__5"><ins>Ksh {{number_format($product->price)}}</ins></span>
             </div>
